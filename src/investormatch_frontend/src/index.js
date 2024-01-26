841533f45317edf0a4b77b7c3631f4abb9336a94
njerimuiruri <faith.muiruri@strathore.edu>
2024-01-26 06:54:25 -0500

    First commit

---
+++ src/investormatch_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { investormatch_backend } from "../../declarations/investormatch_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await investormatch_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
