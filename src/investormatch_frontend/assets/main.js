27364cf9026b5c49de2dff12c89c84e4146891e4
njerimuiruri <faith.muiruri@strathore.edu>
2024-01-29 13:59:08 -0500

    Second commit

--- src/investormatch_frontend/assets/main.js
+++ src/investormatch_frontend/assets/main.js
@@ -27,6 +27,17 @@
         }
       }
     }
+    
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
   
     /**
      * Easy on scroll event listener 
